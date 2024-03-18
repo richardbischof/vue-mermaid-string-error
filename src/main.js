235da87c676db62223aaf098d1e213de7584b70e
Richard Bischof <richard.bischof@lgln.niedersachsen.de>
2024-03-18 07:48:58 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueMermaidString from 'vue-mermaid-string'
+
+createApp(App).use(VueMermaidString).mount('#app')
+
+// createApp(App).mount('#app')
